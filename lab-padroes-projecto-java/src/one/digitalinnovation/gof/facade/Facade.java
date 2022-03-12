29982c5f22ead970d588a797409651e78db31f37
Kevin Lourenço <90475097+View10@users.noreply.github.com>
2022-03-12 17:30:40 +0100

    Add files via upload

---
+++ lab-padroes-projecto-java/src/one/digitalinnovation/gof/facade/Facade.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.gof.facade;
+
+import subsistema1.crm.CrmServise;
+import subsistema2.cep.CepApi;
+
+public class Facade {
+
+    public void migrarCliente(String nome, String cep){
+        String cidade = CepApi.getInstancia().recuperarCidade(cep);
+        String estado = CepApi.getInstancia().recuperarEstado(cep);
+
+        CrmServise.gravarCliente(nome, cep, cidade, estado);
+    }
+}
---
+++ lab-padroes-projecto-java/src/one/digitalinnovation/gof/singleton/SingletonLazyHolder.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.gof.singleton;
+/**
+ * Singleton "Lazy Holder".
+ *
+ * @author kevin
+ */
+public class SingletonLazyHolder {
+    private static class Holder{
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+
+
+    private SingletonLazyHolder() {
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia() {
+        return Holder.instancia;
+    }
+}
